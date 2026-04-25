5c54d6e0bab0c088f999cbabdb6d1959e3b79b63
Артем <artem.bond2017@mail.ru>
2026-04-25 03:00:10 +0300

    add auto create mock users after server startup

--- app/main.py
+++ app/main.py
@@ -5,8 +5,11 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi_healthchecks.api.router import HealthcheckRouter, Probe
 from fastapi_healthchecks.checks.postgres import PostgreSqlCheck
 from fastapi_healthchecks.checks.redis import RedisCheck
+from sqlalchemy import select
 
 from app.api.api_router import main_router as router
+from app.data.models import Users
+from app.data.session import AsyncSessionLocal
 
 from .config import (
     DATABASE_HOST,
@@ -14,18 +17,33 @@ from .config import (
     DATABASE_PORT,
     POSTGRES_PASSWORD,
     POSTGRES_USER,
+    REDIS_HOST,
     REDIS_PORT,
-    REDIS_HOST
 )
 from .data.models import Base
 from .data.session import engine
 
 
+async def create_initial_users():
+    async with AsyncSessionLocal() as session:
+        users_data = [
+            {"id": "f2d51619-ee0d-40e7-86a9-c6cf1314c3b6", "username": "user1"},
+            {"id": "a0c655d3-7065-4e0c-aea6-fa0ad79c59df", "username": "user2"}
+        ]
+        for user_info in users_data:
+            stmt = select(Users).where(Users.id == user_info["id"])
+            result = await session.execute(stmt)
+            if not result.scalar_one_or_none():
+                session.add(Users(**user_info))
+        await session.commit()
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):  # noqa: ARG001
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+    await create_initial_users()
     yield
+
 
 app = FastAPI(lifespan=lifespan)
 
